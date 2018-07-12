72d78891f2e185d434bb6202010e86480b222b9f
llienofdoom <neill.vermaak@gmail.com>
2018-07-12 14:33:15 +0200

    moved to network drive, renamed houdiniHythonRender

--- luma_houdiniHythonRender-IFD.py
+++ luma_houdiniHythonRender-IFD.py
@@ -1,18 +1,18 @@
 import sys, os
 
 # change slashes to forward
-hipfile = sys.argv[1].replace('\\','/')
+hipfile = sys.argv[2].replace('\\','/')
 print hipfile
 
 print "Loading file %s" % (hipfile)
 hou.hipFile.load(hipfile)
 print "Loaded!"
 
-rop = hou.node(sys.argv[2])
+rop = hou.node(sys.argv[4])
 print "Using ROP \"%s\"" % (rop.path())
 
-fs  = int(sys.argv[3])
-fe  = int(sys.argv[4])
+fs  = int(sys.argv[6])
+fe  = int(sys.argv[8])
 print "Setting frame range to %d - %d" % (fs, fe)
 
 # RENDER
