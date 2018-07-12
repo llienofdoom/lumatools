043069e34344fb43eb3816bde52725f577344e97
llienofdoom <neill.vermaak@gmail.com>
2018-07-12 13:16:16 +0200

    added render file

--- luma_houdiniHythonRender.py
+++ luma_houdiniHythonRender.py
@@ -1,8 +1,23 @@
-import sys
+import sys, os
 
-hou.hipFile.load(sys.argv[1])
+# change slashes to forward
+hipfile = sys.argv[1].replace('\\','/')
+print hipfile
+
+print "Loading file %s" % (hipfile)
+hou.hipFile.load(hipfile)
+print "Loaded!"
+
 rop = hou.node(sys.argv[2])
-fs  = sys.argv[3]
-fe  = sys.argv[4]
-rop.render(frame_range=(fs,fe))
+print "Using ROP \"%s\"" % (rop.path())
+
+fs  = int(sys.argv[3])
+fe  = int(sys.argv[4])
+print "Setting frame range to %d - %d" % (fs, fe)
+
+# RENDER
+print "Starting to render...",
+rop.render(frame_range=(fs, fe))
+print "DONE!"
+
 quit()
--- luma_houdiniSendRenderToRP-IFD.py
+++ luma_houdiniSendRenderToRP-IFD.py
@@ -3,8 +3,10 @@ import settings.luma_site_settings
 import os
 import hou
 
+# Load scene file
 hou.hipFile.load("X:/_studiotools/TMP/HQ/mantra_test/mantra_test_103.hip")
 
+# Get list of ROPS to submit
 rop_list = []
 
 rop_out = hou.node("/out/OUT")
@@ -14,5 +16,18 @@ for child in rop_children:
     if type == 'ifd':
         rop_list.append(child)
 
-print rop_list
-print os.environ["HIPNAME"]
+# iterate over ROPS and GO!
+for rop in rop_list:
+    print rop.path(),
+    # get ifd file name
+    parm_diskFile = hou.parm(rop.path() + '/soho_diskfile').unexpandedString()
+    print '\t' + parm_diskFile,
+    # get image file name
+    parm_outputPicture = hou.parm(rop.path() + '/vm_picture').unexpandedString()
+    print '\t' +  parm_outputPicture,
+    # get frame range
+    parm_frameStart = int(hou.evalParm(rop.path() + '/f1'))
+    parm_frameEnd   = int(hou.evalParm(rop.path() + '/f2'))
+    print '\t' +  str(parm_frameStart), str(parm_frameEnd)
+
+hou.releaseLicense()
