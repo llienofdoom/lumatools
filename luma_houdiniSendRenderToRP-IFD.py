8aa6442b5940024bb4c3defb0a10d572831eaaa1
llienofdoom <neill.vermaak@gmail.com>
2018-07-13 18:07:26 +0200

    got ifd part working

--- luma_houdiniSendRenderToRP-IFD.py
+++ luma_houdiniSendRenderToRP-IFD.py
@@ -4,33 +4,39 @@ import os
 import hou
 
 ###############################################################################
-def buildCmdLine():
+g_job = {
+    "name"     : "TESTING - ifd_job_001",
+    "priority" : 5,
+    "rop"      : "/out/rop_hm_base",
+    "frames"   : "1-100",
+    "files"    : "X:/_studiotools/TMP/HQ/mantra_test/mantra_test_103.hip",
+    "outdir"   : ""
+}
+
+###############################################################################
+def buildCmdLine(l_job):
     rpcmd = '"' + os.environ['RP_CMDRC_DIR'] + 'RpRcCmd.exe"'
     cmd = rpcmd
-    cmd += ' -nj_name "test_job1"'
-    cmd += ' -nj_project "TEST_JOB"'
-    cmd += ' -nj_tags "luma","mantra","testing"'
-    cmd += ' -nj_priority 5'
+    cmd += ' -nj_name "%s"' % (l_job['name'])
+    cmd += ' -nj_priority %d' % (l_job['priority'])
     cmd += ' -nj_renderer GenerateIFD/16.5.473'
     cmd += ' -nj_splitmode 2,1'
     cmd += ' -nj_pools "mantra"'
-    # cmd += ' -nj_dependency'
     cmd += ' -nj_paused'
     cmd += ' -retnjid'
-    cmd += ' -rop "/bob/bob"'
-    cmd += ' -frames "1-100"'
-    cmd += ' filename.hip'
+    cmd += ' -rop "%s"' % (l_job['rop'])
+    cmd += ' -frames "%s"' % (l_job['frames'])
+    cmd += ' -outdir "%s"' % (l_job['outdir'])
+    cmd += ' %s' % (l_job['files'])
 
     print cmd
-    # njid = os.system('"' + cmd + '"')
-    # print njid
+    njid = os.system('"' + cmd + '"')
+    print njid
 
 ###############################################################################
 def main():
-    # Load scene file
+    global g_job
     hou.hipFile.load("X:/_studiotools/TMP/HQ/mantra_test/mantra_test_103.hip")
-
-    # Get list of ROPS to submit
     rop_list = []
 
     rop_out = hou.node("/out/OUT")
@@ -40,30 +46,24 @@ def main():
         if type == 'ifd':
             rop_list.append(child)
 
-    # iterate over ROPS and GO!
-    # TESTING
-    # for rop in rop_list:
-    if True:
-        rop = rop_list[0]
-    ## TESTING
-
-        print rop.path(),
-        # get ifd file name
-        parm_diskFile = hou.parm(rop.path() + '/soho_diskfile').unexpandedString()
-        print '\t' + parm_diskFile,
-        # get image file name
+    for rop in rop_list:
+        parm_diskFile      = hou.parm(rop.path() + '/soho_diskfile').unexpandedString()
         parm_outputPicture = hou.parm(rop.path() + '/vm_picture').unexpandedString()
-        print '\t' +  parm_outputPicture,
-        # get frame range
-        parm_frameStart = int(hou.evalParm(rop.path() + '/f1'))
-        parm_frameEnd   = int(hou.evalParm(rop.path() + '/f2'))
-        print '\t' +  str(parm_frameStart), str(parm_frameEnd)
-        buildCmdLine()
+        parm_outputDir     = os.path.dirname(hou.parm(rop.path() + '/vm_picture').eval())
+        parm_frameStart    = int(hou.evalParm(rop.path() + '/f1'))
+        parm_frameEnd      = int(hou.evalParm(rop.path() + '/f2'))
+
+        g_job['name']   = "IFD-GEN : %s - %s - %s" % (os.environ["USERNAME"], os.environ["HIPNAME"], rop.path())
+        g_job['rop']    = rop.path()
+        g_job['frames'] = "%d - %d" % (parm_frameStart, parm_frameEnd)
+        g_job['files']  = os.environ['HIPFILE']
+        g_job['outdir'] = parm_outputDir
+
+        # IFD submission
+        buildCmdLine(g_job)
 
     hou.releaseLicense()
 
 ###############################################################################
-# if __name__ == '__main__':
-#     main()
-
-buildCmdLine()
+if __name__ == '__main__':
+    main()
