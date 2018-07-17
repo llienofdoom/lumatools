84ccfe97e3beb81e1ff424d027eb836c78906d8f
llienofdoom <neill.vermaak@gmail.com>
2018-07-17 13:51:04 +0200

    updated some small bits, given to team! now, wait for the screaming!

--- luma_houdiniSendRenderToRP-IFD.py
+++ luma_houdiniSendRenderToRP-IFD.py
@@ -9,14 +9,16 @@ g_jobIFD = {
     "rop"      : "/out/rop_hm_base",
     "frames"   : "1-100",
     "files"    : "",
-    "outdir"   : ""
+    "outdir"   : "",
+    "paused"   : 0
 }
 g_jobEXR = {
-    "name"     : "IFD-REN - TESTING",
+    "name"     : "",
     "priority" : 5,
     "files"    : "",
     "outdir"   : "",
-    "dep"      : 0
+    "dep"      : 0,
+    "paused"   : 0
 }
 
 ###############################################################################
@@ -27,8 +29,9 @@ def buildCmdLineIFD(l_job):
     cmd += ' -nj_priority %d' % (l_job['priority'])
     cmd += ' -nj_renderer GenerateIFD/16.5.473'
     cmd += ' -nj_splitmode 2,1'
-    cmd += ' -nj_pools "mantra"'
-    cmd += ' -nj_paused'
+    cmd += ' -nj_pools "ifd_gen"'
+    if (l_job['paused'] == 1):
+        cmd += ' -nj_paused'
     cmd += ' -retnjid'
     cmd += ' -rop "%s"' % (l_job['rop'])
     cmd += ' -frames "%s"' % (l_job['frames'])
@@ -45,10 +48,11 @@ def buildCmdLineEXR(l_job):
     cmd += ' "-nj_priority" "%d"' % (l_job['priority'])
     cmd += ' "-nj_renderer" "RenderEXR/16.5.473"'
     cmd += ' "-nj_splitmode" "2,1"'
-    cmd += ' "-nj_pools" "mantra"'
+    cmd += ' "-nj_pools" "ifd_ren"'
     cmd += ' "-nj_dependency" "%d"' % (l_job['dep'])
     cmd += ' "-nj_deptype" "0"'
-    cmd += ' "-nj_paused"'
+    if (l_job['paused'] == 1):
+        cmd += ' -nj_paused'
     cmd += ' "-outdir" "%s"' % (l_job['outdir'])
     cmd += ' %s' % (l_job['files'])
     print cmd
@@ -59,16 +63,17 @@ def buildCmdLineEXR(l_job):
     os.system( tempfile_path )
     os.remove(tempfile_path)
 
-
 ###############################################################################
 def main():
-    if (len(sys.argv) != 2):
+    if (len(sys.argv) != 3):
         exit(1)
 
     global g_jobIFD, g_jobEXR
+
     print 'Loading File...',
     hou.hipFile.load(sys.argv[1], ignore_load_warnings=True)
     print 'DONE!'
+
     rop_list = []
     rop_out = hou.node("/out/OUT")
     rop_children = rop_out.inputAncestors()
@@ -88,6 +93,7 @@ def main():
         g_jobIFD['frames'] = "%d - %d" % (parm_frameStart, parm_frameEnd)
         g_jobIFD['files']  = os.environ['HIPFILE']
         g_jobIFD['outdir'] = parm_outputDir_IFD
+        g_jobIFD['paused'] = int(sys.argv[2])
         # IFD submission
         njid = buildCmdLineIFD(g_jobIFD)
 
@@ -98,6 +104,7 @@ def main():
         g_jobEXR['files']  = files
         g_jobEXR['outdir'] = parm_outputDir_EXR
         g_jobEXR['dep']    = njid
+        g_jobEXR['paused'] = int(sys.argv[2])
         # EXR submission
         buildCmdLineEXR(g_jobEXR)
 
@@ -106,3 +113,4 @@ def main():
 ###############################################################################
 if __name__ == '__main__':
     main()
+    # raw_input('\n\npress enter...')
