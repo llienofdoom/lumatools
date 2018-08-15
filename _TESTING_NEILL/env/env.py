5508f3460a86d50108acf6e57dd56749c1ed5267
llienofdoom <neill.vermaak@gmail.com>
2018-08-15 14:15:14 +0200

    finally working!!!

--- _TESTING_NEILL/env/env.py
+++ _TESTING_NEILL/env/env.py
@@ -1,20 +1,27 @@
 import logging
 import platform
 import os
+import time
 
 # Setup Logging ###############################################################
 logging.basicConfig(
     # filename='neill.log',
-    level=logging.DEBUG, # DEBUG | INFO | WARNING | ERROR | CRITICAL
+    level=logging.DEBUG,
+    # level=logging.INFO,
+    # level=logging.WARNING,
+    # level=logging.ERROR,
+    # level=logging.CRITICAL,
     # format='LUMA-%(asctime)s-%(levelname)s :: %(message)s'
     format='LUMA-%(asctime)s :: %(message)s'
 )
 logging.info('Setting up main environment...')
 # DEBUG - Print env ###########################################################
 def print_env(env):
+    logging.debug('#'*80)
     logging.debug('Printing environment:')
     for key, value in env.iteritems():
         logging.debug('ENV : %s = %s', key, value)
+    logging.debug('#' * 80)
 # Test which os ###############################################################
 os_win = 0
 os_cyg = 0
@@ -33,19 +40,29 @@ if 'darwin' in platform.system().lower():
     os_mac = 1
     logging.info('OS type set to MAC.')
 ###############################################################################
-def write_exe():
-    exe_file_path = os.environ['TEMP'] + os.sep + 'LUMA_CMD.bat'
+def write_exe(cmd, env):
+    exe_file_path = os.environ['TEMP'] + os.sep + 'LUMA_CMD_' + str(time.time()) + '.bat'
     logging.info('Saving cmd file to %s.', exe_file_path)
     env_set       = 'export'
     if os_win:
         env_set = 'SET'
-
+    exe_file = open(exe_file_path, 'w')
+    for key, value in env.iteritems():
+        exe_file.write('%s %s=%s\n' % (env_set, key, value))
+    exe_file.write('\n')
+    if os_win:
+        exe_file.write('start %s\n' % (cmd))
+    else:
+        exe_file.write('%s\n' % (cmd))
+    exe_file.close()
     return exe_file_path
 
 # Setup global env ############################################################
 LA_ENV = {}
 if os_win:
     LA_ENV['ROOT'] = 'H:'
+if os_cyg:
+    LA_ENV['ROOT'] = '/mnt/h'
 LA_ENV['SETTINGS'] = LA_ENV['ROOT'] + '/_distros/_lumatools/lumatools/_TESTING_NEILL/settings'
 # OCIO
 LA_ENV['OCIO'] = LA_ENV['ROOT'] + '/SITE/ocio/nuke-default/config.ocio'
--- _TESTING_NEILL/la_hou_fx.py
+++ _TESTING_NEILL/la_hou_fx.py
@@ -1,8 +1,17 @@
-# import subprocess
+import subprocess, os
 from env.env_houdini import *
 
 logging.info('Writing Environment to file...')
-exe_file = write_exe()
+exe_file = write_exe('houdinicore', LA_ENV)
 logging.info('Done!')
 
 logging.info('Starting Houdini FX.')
+print_env(LA_ENV)
+if os_win:
+    # pass
+    subprocess.call([exe_file], shell=True)
+else:
+    # pass
+    subprocess.call(['/bin/sh', exe_file], shell=True)
+
+os.remove(exe_file)
