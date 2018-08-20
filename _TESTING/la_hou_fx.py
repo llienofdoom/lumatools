934dc746af77ea18a9f6d2f006197ce5791fb206
llienofdoom <neill.vermaak@gmail.com>
2018-08-20 11:30:18 +0200

    updated stable version to 16.5.536, and cleaned up folder

---
+++ _TESTING/la_hou_fx.py
@@ -0,0 +1,12 @@
+from env.env_houdini import *
+
+args = ' '.join(sys.argv[1:])
+
+logging.info('Writing Environment to file...')
+exe_file = write_exe('houdinifx ' + args, LA_ENV)
+logging.info('Done!')
+
+logging.info('Starting Houdini FX.')
+logging.info('Using args : %s' % args)
+
+run_exe(LA_ENV, exe_file)
