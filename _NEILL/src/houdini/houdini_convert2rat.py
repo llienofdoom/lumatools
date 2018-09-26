e61ffff13e6840ad0abeeed10ea75cc4b8b46f80
llienofdoom <neill.vermaak@gmail.com>
2018-09-26 14:50:58 +0200

    updated for multiple files

--- _NEILL/src/houdini/houdini_convert2rat.py
+++ _NEILL/src/houdini/houdini_convert2rat.py
@@ -4,8 +4,9 @@ from houdini_base import *
 setHoudiniEnv()
 
 args = sys.argv[1:]
-inFile = str(args[0])
-outFile = inFile.split('.')[0] + '.rat'
-cmd = env['HB'] + '/iconvert %s %s' % (inFile, outFile)
-print 'Starting', cmd
-la_utils.runCmd(cmd, env)
+for current in args:
+    inFile = str(current)
+    outFile = inFile.split('.')[0] + '.rat'
+    cmd = env['HB'] + '/iconvert %s %s' % (inFile, outFile)
+    print 'Starting', cmd
+    la_utils.runCmd(cmd, env)
