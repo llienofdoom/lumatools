b82198e333af802028b31a0aace93121af298e17
llienofdoom <neill.vermaak@gmail.com>
2018-10-24 18:30:57 +0200

    docs

--- _NEILL/docs/make_docs.cmd
+++ _NEILL/docs/make_docs.cmd
@@ -3,4 +3,4 @@ CALL %LA_ROOT%\_%LA_BRANCH%\bin\win\la_global.cmd
 
 cd /d %LA_ROOT%\_%LA_BRANCH%\docs\src
 
-H:\_distros\_lumatools\latex\miktex\texmfs\install\miktex\bin\pdflatex.exe -quiet -aux-directory=.\tmp -output-directory=.. -job-name=lumatools main.tex
+H:\_distros\_lumatools\latex\miktex\texmfs\install\miktex\bin\pdflatex.exe -quiet -aux-directory=.\tmp -output-directory=.. -job-name=lumatools _main.tex
