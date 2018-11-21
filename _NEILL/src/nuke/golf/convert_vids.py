684721195e979ade16d7da4c06318e84ac0cd72f
llienofdoom <neill.vermaak@gmail.com>
2018-11-21 14:40:57 +0200

    updated golf convertsion script etc

--- _NEILL/src/nuke/golf/convert_vids.py
+++ _NEILL/src/nuke/golf/convert_vids.py
@@ -17,15 +17,29 @@ for folder in folders:
                 new_name   = os.path.join(new_folder, name.replace('mov', 'mp4'))
                 if not os.path.exists(new_folder):
                     os.makedirs(new_folder)
-                cmd = ffmpeg + ' -y -hide_banner -loglevel panic -threads 8'
+                cmd = ffmpeg + ' -y ' #-hide_banner -loglevel panic -threads 8'
                 cmd += ' -gamma 2.2'
                 cmd += ' -i %s' % old_name
                 cmd += ' -pix_fmt yuv420p'
                 cmd += ' -c:v libx264'
-                cmd += ' -crf 20'
+                cmd += ' -crf 25'
+                # cmd += ' -vf scale -1:240:flags=bicubic'
+                cmd += ' -vf scale=-2:240'
                 cmd += ' %s' % new_name
                 cmd = cmd.replace('\\', '/')
                 # print cmd
                 print 'Converting %s' % old_name ,
                 la_utils.runCmd(cmd)
                 print 'DONE'
+# Scaling algorithms
+# fast_bilinear
+# bilinear
+# bicubic
+# experimental
+# neighbor
+# area
+# bicublin
+# gauss
+# sinc
+# lanczos
+# spline
