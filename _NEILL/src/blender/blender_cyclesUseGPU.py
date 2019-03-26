8f993f88aa578424b335943da6f20f5d3b325755
llienofdoom <neill.vermaak@gmail.com>
2019-03-26 13:43:29 +0200

    added houdini 17.5

--- _NEILL/src/blender/blender_cyclesUseGPU.py
+++ _NEILL/src/blender/blender_cyclesUseGPU.py
@@ -1,9 +1,19 @@
 import bpy
-bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
-size = len(bpy.context.user_preferences.addons['cycles'].preferences.devices)
-for device in range(0, size):
-    name   = bpy.context.user_preferences.addons['cycles'].preferences.devices[device].name
-    in_use = bpy.context.user_preferences.addons['cycles'].preferences.devices[device].use
-    print('Current = %s = %s' % (name, in_use))
-    bpy.context.user_preferences.addons['cycles'].preferences.devices[device].use = True
-    bpy.ops.wm.save_userpref()
+
+scene = bpy.context.scene
+scene.cycles.device = 'GPU'
+
+prefs = bpy.context.user_preferences
+cprefs = prefs.addons['cycles'].preferences
+
+# Attempt to set GPU device types if available
+for compute_device_type in ('CUDA', 'OPENCL', 'NONE'):
+    try:
+        cprefs.compute_device_type = compute_device_type
+        break
+    except TypeError:
+        pass
+
+# Enable all CPU and GPU devices
+for device in cprefs.devices:
+    device.use = True
