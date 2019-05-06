1c52af2fae671a3c2ba499e2198e12a6bc1da538
llienofdoom <neill.vermaak@gmail.com>
2019-05-06 09:32:38 +0200

    updated houdini and redshift for win

--- _NEILL/bin/win/TESTING_houdini_core.cmd
+++ _NEILL/bin/win/TESTING_houdini_core.cmd
@@ -3,5 +3,5 @@ CALL %LA_ROOT%\_%LA_BRANCH%\bin\win\la_global.cmd
 
 cd /d %PYTHONPATH%\houdini
 SET LA_HOU_CURRENT=0015
-SET OCIO=H:\SITE\ocio\aces_1.0.3\config.ocio
+REM SET OCIO=H:\SITE\ocio\aces_1.0.3\config.ocio
 python houdini_core.py %*
